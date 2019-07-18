bbc72a5fae74847f2885e29ea14617f679e84295
ganzabeans <52638560+ganzabeans@users.noreply.github.com>
2019-07-18 16:36:59 -0700

    Update BoxRender.cs

--- BoxRender.cs
+++ BoxRender.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,78 +21,25 @@ public class BoxRender : MonoBehaviour
     //  This function changes the cube color
     public void SetColor(int num)
     {
-        switch (num)
-        {
-            case 0:
-                rend.material.shader = Shader.Find("_Color");               // 0 is white
-                rend.material.SetColor("_Color", Color.white);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.white);
-                break;
-            case 1:
-                rend.material.shader = Shader.Find("_Color");               // 1 is blue
-                rend.material.SetColor("_Color", Color.blue);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.blue);
-                break;
-            case 2:                                                         // 2 is green
-                rend.material.shader = Shader.Find("_Color");
-                rend.material.SetColor("_Color", Color.green);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.green);
-                break;
-            case 3:
-                rend.material.shader = Shader.Find("_Color");               // 3 is red
-                rend.material.SetColor("_Color", Color.red);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.red);
-                break;
-            case 4:
-                rend.material.shader = Shader.Find("_Color");               // 4 is purple
-                rend.material.SetColor("_Color", Color.HSVToRGB(267, 74f, 59f)); 
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.HSVToRGB(267, 74f, 59f));
-                break;
-            case 5:
-                rend.material.shader = Shader.Find("_Color");               // 5 is maroon
-                rend.material.SetColor("_Color", Color.HSVToRGB(338f, 100f, 27f)); 
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.HSVToRGB(338f, 100f, 27f));
-                break;
-            case 6:
-                rend.material.shader = Shader.Find("_Color");               // 6 is turquoise 
-                rend.material.SetColor("_Color", Color.cyan);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.cyan);
-                break;
-            case 7:
-                rend.material.shader = Shader.Find("_Color");               // 7 is black 
-                rend.material.SetColor("_Color", Color.black);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.black);
-                break;
-            case 8:                                                         // 8 is grey
-                rend.material.shader = Shader.Find("_Color");
-                rend.material.SetColor("_Color", Color.grey);
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.grey);
-                break;
-            case 9: //end game                                              //mines are dark read but look black 
-                rend.material.shader = Shader.Find("_Color");
-                rend.material.SetColor("_Color", Color.HSVToRGB(10f, 100f, 54f));   
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.HSVToRGB(10f, 100f, 54f));
-                break;
-            case 10:
-                //do nothing
-                break;
-            case 11: //start game                                           // start block is yellow
-                rend.material.shader = Shader.Find("_Color");
-                rend.material.SetColor("_Color", Color.yellow); 
-                rend.material.shader = Shader.Find("Specular");
-                rend.material.SetColor("_SpecColor", Color.yellow);
-                break;
+        Color[] colors = new Color[11];
+        colors[0] = Color.white;
+        colors[1] = Color.blue;
+        colors[2] = Color.green;
+        colors[3] = Color.red;
+        colors[4] = Color.HSVToRGB(267, 74f, 59f);      //purple
+        colors[5] = Color.HSVToRGB(338f, 100f, 27f); //maroon
+        colors[6] = Color.cyan;
+        colors[7] = Color.black;
+        colors[8] = Color.grey;
+        colors[9] = Color.HSVToRGB(10f, 100f, 54f); //dark red
+        colors[10] = Color.yellow;
 
+        if ((rend != null) && (num <= 10))
+        {
+            rend.material.shader = Shader.Find("_Color");
+            rend.material.SetColor("_Color", colors[num]);
+            rend.material.shader = Shader.Find("Specular");
+            rend.material.SetColor("_SpecColor", colors[num]);
         }
     }
 }
